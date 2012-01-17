150f0dbd95a555f6a39cf8a229d45852143e06da
Amarjeet Singh Rai <git@arai.dev>
2012-01-17 21:42:07 +0000

    Homepage with posters of films, linking to their pages.

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -29,7 +29,11 @@ class SiteController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
+		// $this->render('index');
+		$dataProvider=new CActiveDataProvider('Film');
+		$this->render('index',array(
+			'dataProvider'=>$dataProvider,
+		));
 	}
 
 	/**
