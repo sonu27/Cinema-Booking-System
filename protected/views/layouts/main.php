968db2246f39b30add2c12e178719fdf13cdf0dd
Amarjeet Singh Rai <git@arai.dev>
2012-02-04 15:27:58 +0000

    New CSS from scratch.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -3,15 +3,9 @@
 <head>
 	<meta charset="utf-8">
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+    <link rel="stylesheet" href=""<?php echo Yii::app()->request->baseUrl; ?>/css/normalise.css" />
+	<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
+	<link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
