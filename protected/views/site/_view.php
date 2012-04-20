7cba44f29cbfd764d16f7d786a9b07ce9903f887
Amarjeet Singh Rai <git@arai.dev>
2012-04-20 16:45:12 +0100

    Fixed hover bug, but Firefox still has it.

--- protected/views/site/_view.php
+++ protected/views/site/_view.php
@@ -1,8 +1,11 @@
 <div class="poster">
-<?php
-$text='<img src="' . Yii::app()->getBaseUrl() . '/images/posters/' . $data->film_id . '.jpg" width="180" alt="' . $data->title . ' Poster" />';
-$span='<span>' . $data->title . '</span>';
-echo CHtml::link($text, array('film/view', 'id'=>$data->film_id));
-echo '<p>' . CHtml::link($span, array('film/view', 'id'=>$data->film_id)) . '</p>';
-?>
+    <div class="posterimg">
+        <?php
+        $text='<img src="' . Yii::app()->getBaseUrl() . '/images/posters/' . $data->film_id . '.jpg" alt="' . $data->title . ' Poster" />';
+        echo CHtml::link($text, array('film/view', 'id'=>$data->film_id));
+        ?>
+        
+    </div>
+    <?php echo '<p>' . CHtml::link($data->title, array('film/view', 'id'=>$data->film_id)) . '</p>'; ?>
+    
 </div>
